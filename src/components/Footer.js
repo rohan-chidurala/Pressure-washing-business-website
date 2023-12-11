9b984cb406fe67d8a12d0b07a77b8afa33151b6c
rohan chidurala <59586662+rohan-chidurala@users.noreply.github.com>
2023-12-11 06:06:02 -0800

    changes to fix bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../styles/Footer.css'; // Make sure this import path is correct
+import { Link } from 'react-router-dom'; // Import Link
+import '../styles/Footer.css';
 
 const Footer = () => {
   return (
@@ -10,9 +11,8 @@ const Footer = () => {
           <p><i className="fas fa-envelope"></i> jspristinepressureclean@gmail.com</p>
         </div>
         <div className="footer-social">
-
+          {/* Social links can go here */}
         </div>
-        
         <div className="footer-action">
           <button className="action-button">Book Your Cleaning</button>
         </div>
@@ -20,7 +20,7 @@ const Footer = () => {
       <div className="footer-bottom">
         <p>© 2023 J's Pristine Pressure Cleaning. All Rights Reserved.</p>
         <div className="footer-links">
-
+          <Link to="/privacy-policy">Privacy Policy</Link> {/* Privacy Policy link */}
         </div>
       </div>
     </footer>
--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -5,52 +5,64 @@ import '../styles/PrivacyPolicy.css'; // Ensure you have the corresponding CSS f
 const PrivacyPolicy = () => {
   return (
     <div className="privacy-policy-container">
-      <h1>Privacy Policy</h1>
-      <p>Last updated: [Today's Date]</p>
+<h1>Privacy Policy of Js pristine pressure cleaing</h1>
 
-      <h2>Introduction</h2>
-      <p>Your Company Name ("we", "our", or "us") operates the Your Website URL (the "Service").</p>
-      <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data.</p>
+<p>Js pristine pressure cleaing operates the jspressurecleaning.com website, which provides the SERVICE.</p>
 
-      <h2>Information Collection and Use</h2>
-      <p>We collect several different types of information for various purposes to provide and improve our Service to you.</p>
+<p>This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the jspressurecleaning website.</p>
 
-      <h3>Types of Data Collected</h3>
-      <h4>Personal Data</h4>
-      <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you ("Personal Data"). Personally identifiable information may include, but is not limited to:</p>
-      <ul>
-        <li>Email address</li>
-        <li>First name and last name</li>
-        <li>Phone number</li>
-        <li>Address, State, Province, ZIP/Postal code, City</li>
-        <li>Cookies and Usage Data</li>
-      </ul>
+<p>If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
 
-      <h4>Usage Data</h4>
-      <p>We may also collect information on how the Service is accessed and used ("Usage Data"). This Usage Data may include information such as your computer's Internet Protocol address (IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other diagnostic data.</p>
+<p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at jspressurecleaning.com, unless otherwise defined in this Privacy Policy.</p>
 
-      <h4>Tracking & Cookies Data</h4>
-      <p>We use cookies and similar tracking technologies to track the activity on our Service and we hold certain information.</p>
-      <p>Cookies are files with a small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Other tracking technologies are also used such as beacons, tags, and scripts to collect and track information and to improve and analyze our Service.</p>
-      <p>You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</p>
-      
+<h2>Information Collection and Use</h2>
 
-      <h2>Children's Privacy</h2>
-      <p>Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13 years of age. If we become aware that we have collected Personal Data from children without verification of parental consent, we take steps to remove that information from our servers.</p>
+<p>For a better experience while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to your name, phone number, and postal address. The information that we collect will be used to contact or identify you.</p>
 
-      <h2>Changes to This Privacy Policy</h2>
-      <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
-      <p>We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the "effective date" at the top of this Privacy Policy.</p>
-      <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+<h2>Log Data</h2>
 
-      <h2>Contact Us</h2>
-      <p>If you have any questions about this Privacy Policy, please contact us:</p>
-      <ul>
-        <li>By email: [Your Contact Email]</li>
-        <li>By visiting this page on our website: [Your Contact Page URL]</li>
-        <li>By phone number: [Your Contact Number]</li>
-        <li>By mail: [Your Business Address]</li>
-      </ul>
+<p>We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.</p>
+
+<h2>Cookies</h2>
+
+<p>Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer's hard drive.</p>
+
+<p>Our website uses these "cookies" to collection information and to improve our Service. You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our Service.</p>
+
+<h2>Service Providers</h2>
+
+<p>We may employ third-party companies and individuals due to the following reasons:</p>
+
+<ul>
+    <li>To facilitate our Service;</li>
+    <li>To provide the Service on our behalf;</li>
+    <li>To perform Service-related services; or</li>
+    <li>To assist us in analyzing how our Service is used.</li>
+</ul>
+
+<p>We want to inform our Service users that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
+
+<h2>Security</h2>
+
+<p>We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+
+<h2>Links to Other Sites</h2>
+
+<p>Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+
+<p>Children's Privacy</p>
+
+<p>Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.</p>
+
+<h2>Changes to This Privacy Policy</h2>
+
+<p>We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.</p>
+
+<p>Our Privacy Policy was created with the help of the <a href="https://www.privacypolicytemplate.net">Privacy Policy Template</a>.</p>
+
+<h2>Contact Us</h2>
+
+<p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p>
     </div>
   );
 };
